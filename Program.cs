621648f2ccca4b0432e3822b198159201b0ef454
tiagopariz <tiagopariz@gmail.com>
2018-01-02 19:35:26 -0200

    Console Application

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplicationVSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nMy Console App!\n");
+            var key = Console.Read();
+            Console.WriteLine(key.ToString());
+        }
+    }
+}
